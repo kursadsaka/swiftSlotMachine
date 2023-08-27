4414fe881297f53ab9c7c21573e5539cb3706b88
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2023-08-27 14:59:22 +0300

    Initial Commit

---
+++ SlotMachine/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SlotMachine
+//
+//  Created by Kürşad Saka on 27.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Kürşad Saka on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
